2bd8d3a063022ce77c218aaa1adf3cbe8f8b9c83
leny <leny@172.17.192.100>
2018-03-30 01:09:28 +0900

    first_draft

--- src/main/java/edu/handong/csee/java/lab06/Bug_infestation.java
+++ src/main/java/edu/handong/csee/java/lab06/Bug_infestation.java
@@ -7,16 +7,36 @@ public class Bug_infestation {
 	public static final double ONE_BUG_VOLUME = 0.002;
 	
 	public static void main(String[] args) {
-		int houseVolume;
+		double houseVolume;
 		int startPopulation;
-		Bug_infestation bug = new Bug_infestation();
-		
+			
 		Scanner keyboard = new Scanner(System.in);
-		
+			
 		System.out.print("Enter the total volume of your house in cubic feet: ");
 		houseVolume = keyboard.nextInt();
 		
 		System.out.print("Enter the estimated number of roaches in your house: ");
 		startPopulation = keyboard.nextInt();
+		
+		int population = startPopulation;
+		
+		int totalBugVolume = (int)(population * GROWTH_RATE);
+		double newBugVolume;
+		int newBugs;
+		int countWeeks = 0;
+		
+	    while (totalBugVolume < houseVolume){
+    	    newBugs = (int)(population * GROWTH_RATE);
+    	    newBugVolume = newBugs * ONE_BUG_VOLUME;
+    	    population = population + newBugs;
+    	    totalBugVolume = (int)(totalBugVolume + newBugVolume);
+    	    countWeeks = countWeeks + 1;
+        }
+		
+	    System.out.print("Starting with a roach population of " + startPopulation);
+	    System.out.print("and a house with a volume of " + houseVolume + " cubic feet, after " + countWeeks + " weeks,");
+	    System.out.print("the house will be filled with " + population + " roaches.");
+	    System.out.print("They will fill a volume of " + totalBugVolume + " cubic feet");
+	    System.out.print("Better call Debugging Experts Inc.");
 	}
 }
